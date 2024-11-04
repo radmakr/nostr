fd2d9b8a5d5b53d629219026a6774662fd5bc9a5
Yuki Kishimoto <yukikishimoto@protonmail.com>
2024-11-04 16:06:06 +0100

    c: add `keys_parse`
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

--- bindings/nostr-sdk-c/examples/keys.c
+++ bindings/nostr-sdk-c/examples/keys.c
@@ -11,5 +11,11 @@ int main() {
     const char* public_key = keys_public_key(keys);
     printf("Public key: %s\n", public_key);
 
+    // Parse keys
+    Keys* keysParse = keys_generate();
+    keys_parse(keysParse, "nsec1j4c6269y9w0q2er2xjw8sv2ehyrtfxq3jwgdlxj6qfn8z4gjsq5qfvfk99");
+    const char* public_key_parse = keys_public_key(keysParse);
+    printf("Public key: %s\n", public_key_parse);
+
     return 0;
 }
--- bindings/nostr-sdk-c/src/protocol/key/mod.rs
+++ bindings/nostr-sdk-c/src/protocol/key/mod.rs
@@ -6,31 +6,35 @@ use std::ffi::{c_char, CStr, CString};
 
 use nostr_sdk::nostr::key;
 
-use crate::error::{into_err, Result};
-
 pub struct Keys {
     inner: key::Keys,
 }
 
-#[no_mangle]
-pub extern "C" fn keys_generate() -> *const Keys {
-    Box::into_raw(Box::new(Keys {
-        inner: key::Keys::generate(),
-    }))
-}
+impl Keys {
+    #[no_mangle]
+    pub extern "C" fn keys_generate() -> *mut Keys {
+        Box::into_raw(Box::new(Keys {
+            inner: key::Keys::generate(),
+        }))
+    }
 
-#[no_mangle]
-pub unsafe extern "C" fn keys_parse(secret_key: *const c_char) -> Result<Keys> {
-    let secret_key: &str = CStr::from_ptr(secret_key).to_str().map_err(into_err)?;
-    Ok(Keys {
-        inner: key::Keys::parse(secret_key).map_err(into_err)?,
-    })
-}
+    #[no_mangle]
+    pub unsafe extern "C" fn keys_parse(keys: &mut Keys, secret_key: *const c_char) -> u8 {
+        let secret_key: &str = CStr::from_ptr(secret_key).to_str().unwrap();
+        match key::Keys::parse(secret_key) {
+            Ok(k) => {
+                *keys = Keys { inner: k };
+                0
+            }
+            Err(..) => 1,
+        }
+    }
 
-/// Get public key from Keys
-#[no_mangle]
-pub extern "C" fn keys_public_key(keys: &Keys) -> *const c_char {
-    let public_key = keys.inner.public_key.to_string();
-    let c_string = CString::new(public_key).unwrap();
-    c_string.into_raw()
+    /// Get public key from Keys
+    #[no_mangle]
+    pub extern "C" fn keys_public_key(keys: &Keys) -> *const c_char {
+        let public_key = keys.inner.public_key.to_string();
+        let c_string = CString::new(public_key).unwrap();
+        c_string.into_raw()
+    }
 }
